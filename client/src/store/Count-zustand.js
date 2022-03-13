c429eeb08b2a423b067796915f02fb8c50f48cc0
happy5happy5 <zombil8731@gmail.com>
2022-03-13 12:41:57 +0900

    feat : Zustand persist

--- client/src/store/Count-zustand.js
+++ client/src/store/Count-zustand.js
@@ -1,23 +1,32 @@
 import create from "Zustand";
-// import { persist } from "zustand/middleware"
+import { persist } from "zustand/middleware";
 
-const useStore = create((set) => ({
-  count: 0,
-  증가() {
-    set((state) => ({ count: state.count + 1 }));
-  },
-  감소() {
-    set((state) => ({ count: state.count - 1 }));
-  },
-  곱하기2() {
-    set((state) => ({ count: state.count * 2 }));
-  },
-  나누기2() {
-    set((state) => ({ count: state.count / 2 }));
-  },
-  리셋() {
-    set((state) => ({ count: 0 }));
-  },
-}));
+const useStore = create(
+  persist(
+    (set) => ({
+      count: 0,
+      증가() {
+        set((state) => ({ count: state.count + 1 }));
+      },
+      감소() {
+        set((state) => ({ count: state.count - 1 }));
+      },
+      곱하기2() {
+        set((state) => ({ count: state.count * 2 }));
+      },
+      나누기2() {
+        set((state) => ({ count: state.count / 2 }));
+      },
+      리셋() {
+        set((_) => ({ count: 0 }));
+      },
+    }),
+    {
+      name: "count in session u can ckeck via window.localStorage",
+      getStorage: () => sessionStorage, //세션에 저장
+      //   getStorage: () => localStorage, //로컬에 저장
+    }
+  )
+);
 
 export default useStore;
